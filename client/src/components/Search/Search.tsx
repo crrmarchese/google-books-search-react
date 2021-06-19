0dd7a6d81b310c6d20a2f1d3f9b216a9e91f197b
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-06-18 20:01:38 -0400

    Add search input and button

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {InputText} from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import "./styles.css";
 
 function Search() {
     return (
         <div>
-            <div className="p-fluid">
-                <div className="p-field">
+            <div>
+                <div className="p-field p-fluid">
                     <label htmlFor="bookname">Book</label>
                     <InputText id="bookname" type="text"/>
+                </div>
+                <div className="p-d-flex p-jc-end">
+                    <Button label="Search" className="gb-btn-search" />
                 </div>
             </div>
         </div>

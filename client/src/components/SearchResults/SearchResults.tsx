c228b074c5673dc193984d90225570dcb7a1e011
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-06-19 20:00:41 -0400

    Style search Results heading

--- client/src/components/SearchResults/SearchResults.tsx
+++ client/src/components/SearchResults/SearchResults.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import ResultsList from '../ResultsList/ResultsList';
 import './styles.css';
 
 function SearchResults() {
     return (
         <div className="p-grid p-container">
-            <div className="p-col">
-                <h2>Results</h2>
+            <div className="p-col-12">
+                <div className="card">
+                    <div className="p-orderlist p-orderlist-list-container">
+                        <div className="p-orderlist-header">
+                        <h2>Results</h2>
+                        </div>
+                    </div> 
+                    <ResultsList />
+                </div>
+                
             </div>
         </div>
     )

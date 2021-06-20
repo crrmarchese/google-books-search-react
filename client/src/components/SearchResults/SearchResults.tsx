87507fdb7fbccb2f30e8665eb56f7753cbc5abf8
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-06-20 18:54:54 -0400

    Results header full width; change bg color

--- client/src/components/SearchResults/SearchResults.tsx
+++ client/src/components/SearchResults/SearchResults.tsx
@@ -6,10 +6,10 @@ function SearchResults() {
     return (
         <div className="p-grid p-container">
             <div className="p-col-12">
-                <div className="card">
+                <div className="search-results card">
                     <div className="p-orderlist p-orderlist-list-container">
-                        <div className="p-orderlist-header">
-                        <h2>Results</h2>
+                        <div className="p-orderlist-header">   
+                            <h2>Results</h2>      
                         </div>
                     </div> 
                     <ResultsList />

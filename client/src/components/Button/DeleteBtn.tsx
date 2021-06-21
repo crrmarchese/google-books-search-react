ee0ce7d35c4d17a0a4236afd46f8650fdf9dbfb9
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-06-21 19:39:56 -0400

    Create Delete button component

---
+++ client/src/components/Button/DeleteBtn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'primereact/button';
+import './style.css';
+
+function DeleteBtn() {
+    return (
+        <>
+          <Button label="Delete" icon="pi pi-trash" role="button" />  
+        </>
+    )
+}
+
+export default DeleteBtn

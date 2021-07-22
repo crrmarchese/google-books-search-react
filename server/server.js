318fecdc11b9ff5e4de2f6fd007017312e696b2c
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-07-22 16:06:36 -0400

    Add  then/catch to mongoose.connect, remove local host

--- server/server.js
+++ server/server.js
@@ -17,11 +17,17 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactgooglebooks", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
-});
+})
+.then(() => {
+  console.log("DB Connected");
+})
+.catch(err => {
+  console.log("error", err);
+})
 
 // Start the API server
 app.listen(PORT, function() {

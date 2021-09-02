6273f46171a71f2cea196ee48c29d1216dbe26dd
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-09-02 15:08:02 -0400

    Add parameters for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require('dotenv').config();
 
 const mongoose = require("mongoose");
@@ -9,10 +10,7 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 // Add routes, both API and view
 app.use(routes);
 
@@ -28,6 +26,14 @@ mongoose.connect(process.env.MONGODB_URI, {
 .catch(err => {
   console.log("error", err);
 })
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    });
+}
 
 // Start the API server
 app.listen(PORT, function() {

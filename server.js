34ba94d76dd1bdfc3d4274aa489993a2bf6a8ad7
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-09-09 12:42:14 -0400

    Add heroku scripts

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
     });
 }
 
